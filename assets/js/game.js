317698e093c6b5fe49006826a969c7a7422d4c34
Jolo <jolocabrera@yahoo.com>
2022-03-29 19:01:59 -0700

    add breaks into loops

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,28 +17,45 @@ var enemyAttack = 12;
 
 var fight = function(enemyName) {
   // repeat and execute as long as the enemy-robot is alive
-  while(enemyHealth > 0) {
-
+  while(playerHealth > 0 && enemyHealth > 0) { 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    
+    // if player chooses to skip
+    if (promptFight === "skip" || promptFight === "SKIP") {
+      // confirm player wants to skip
+      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+      
+      // if yes (true), leave fight
+      if (confirmSkip) {
+        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        // subtract money from playerMoney for skipping
+        playerMoney = playerMoney - 10;
+        console.log("playerMoney", playerMoney);
+        break;
+      }
+    }
+      
+        // remove enemy's health by subtracting the amount set in the playerAttack variable.
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(
+          playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+          );
+          
+          // check enemy's health
+          if (enemyHealth <= 0) {
+            window.alert(enemyName + " has died!");
 
-    // if player chooses to fight, then fight
-    if (promptFight === "fight" || promptFight === "FIGHT") {
-      // remove enemy's health by subtracting the amount set in the playerAttack variable.
-      enemyHealth = enemyHealth - playerAttack;
-      console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-        );
-        
-        // check enemy's health
-        if (enemyHealth <= 0) {
-          window.alert(enemyName + " has died!");
-        }
-        else {
-          window.alert(enemyName + " still has " + enemyHealth + " health left.");
-        }
-        
-        //remove player's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+            // award player money for winning
+            playerMoney = playerMoney + 20;
+            // leave while() loop since enemy is dead
+            break;
+          }
+          else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+          }
+          
+          //remove player's health by subtracting the amount set in the enemyAttack variable
+          playerHealth = playerHealth - enemyAttack;
         console.log(
           enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
           );
@@ -46,33 +63,12 @@ var fight = function(enemyName) {
           // Check player's health
           if (playerHealth <= 0) {
             window.alert(playerName + " has died!");
+            // leave while() loop if player is dead
+            break;
           }
           else {
             window.alert(playerName + " still has " + playerHealth + " health left.");
           }
-    }
-        
-        // if player chooses to skip
-    else if (promptFight === "skip" || promptFight === "SKIP") {
-          // confirm player wants to skip
-          var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-          
-          // if yes (true), leave fight
-          if (confirmSkip) {
-            window.alert(playerName + " has decided to skip this fight. Goodbye!");
-            // subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 2;
-          }
-          
-          // if no (false), ask question again by running fight() again
-          else {
-            fight();
-          }
-    }
-        
-    else {
-          window.alert("You need to choose a valid option. Try again!");
-    }
   }
 };
     
